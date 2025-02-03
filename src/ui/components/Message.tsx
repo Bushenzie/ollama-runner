fd8d64cc944e8146de88c0d709ab97f2ecc3580e
Daniel Buchal <daniel.buchal01@gmail.com>
2025-02-03 11:13:52 +0100

    feat: Message copying

--- src/ui/components/Message.tsx
+++ src/ui/components/Message.tsx
@@ -4,8 +4,14 @@ import SyntaxHighlighter from 'react-syntax-highlighter'
 import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import remarkGfm from 'remark-gfm'
 import { Message as MessageProps } from "../types/message"
+import { Copy } from "lucide-react";
 
 export const Message: React.FC<MessageProps> = ({ content, role }) => {
+
+    const handleCopy = async (text: string) => {
+        await navigator.clipboard.writeText(text);
+    }
+
     return (
         <div className={clsx("chat", role === "assistant" ? "chat-start" : "chat-end")}>
             <div className={clsx("chat-bubble", role === "user" && "chat-bubble-primary")}>
@@ -19,17 +25,24 @@ export const Message: React.FC<MessageProps> = ({ content, role }) => {
                             const isCodeBlock = className?.includes("language");
                             const language = className?.split("-")[1]
                             return isCodeBlock ? (
-                                <SyntaxHighlighter
-                                    children={String(children)}
-                                    language={language}
-                                    style={atomOneDark}
-                                />
+                                <div className="p-2 flex flex-col bg-zinc-950">
+                                    <div className="flex p-2 justify-between">
+                                        <Copy className="cursor-pointer" onClick={() => handleCopy(String(children))} />
+                                        <span className="capitalize self-end">{language}</span>
+                                    </div>
+                                    <SyntaxHighlighter
+                                        children={String(children)}
+                                        language={language}
+                                        style={atomOneDark}
+                                    />
+                                </div>
                             ) : (
                                 <code {...rest} className={clsx("inline text-xs bg-zinc-700 p-1", className)}>
                                     {children}
                                 </code>
                             )
                         },
+
                         h1(props) {
                             return <h1 className="text-3xl leading-loose font-bold">{props.children}</h1>
                         },
